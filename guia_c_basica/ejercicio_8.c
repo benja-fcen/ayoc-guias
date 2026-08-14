f50dc0206b268848167906e9390ddc542297d0d6
benja-fcen <rtb.fcen@gmail.com>
2026-08-14 16:57:44 -0300

    Ejercicio 8

---
+++ guia_c_basica/ejercicio_8.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+	int i = 5;
+	int j = 10;
+	printf("El operador preincremento ++i, primero incrementa i y luego devuelve el valor de i:\n"
+		"Si tenemos que i vale %d", i);
+	printf(" vamos a tener que ++i vale %d", ++i);
+	printf(" e i vale %d\n", i);
+	
+
+
+}
