fd7d9a89aa452d65d14d272e783bd1fbca190e82
benja-fcen <rtb.fcen@gmail.com>
2026-08-14 21:30:44 -0300

    agregado operador de postincremento

--- guia_c_basica/ejercicio_8.c
+++ guia_c_basica/ejercicio_8.c
@@ -3,11 +3,13 @@
 int main() {
 	int i = 5;
 	int j = 10;
-	printf("El operador preincremento ++i, primero incrementa i y luego devuelve el valor de i:\n"
+	printf("El operador preincremento ++i, primero incrementa i y luego devuelve su valor:\n"
 		"Si tenemos que i vale %d", i);
 	printf(" vamos a tener que ++i vale %d", ++i);
-	printf(" e i vale %d\n", i);
-	
-
+	printf(" luego de la operación i vale %d\n", i);
+	printf("El operador postincremento j++, primero devuelve el valor de j y luego lo incrementa:\n"
+		"Si tenemos que j vale %d", j);
+	printf(" vamos a tener que j++ vale %d", j++);
+	printf(" luego de la operación j vale %d\n", j);
 
 }
