5c979a81ea686f392ab26ca85be0d36849ad9521
benja-fcen <rtb.fcen@gmail.com>
2026-08-15 12:10:09 -0300

    generalización

--- guia_c_basica/ejercicio_9.c
+++ guia_c_basica/ejercicio_9.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <stdint.h>
+#define SIZE_IN_BITS sizeof(int32_t) * 8
+
 
 int main() {
-	uint32_t a, b;
-	scanf("%u%u", &a, &b);
-	a = a >> 29;
-	b &= 0x7;
+	int32_t a, b;
+	scanf("%d%d", &a, &b);
+	a = (a >> (SIZE_IN_BITS - 3)) & 0x07;
+	b &= 0x07;
 	printf(a == b ? "iguales\n" : "diferentes\n");
 }
