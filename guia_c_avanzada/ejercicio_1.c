fb672c4ba0e44c592db2167d90e24ed7500fbe26
benja-fcen <rtb.fcen@gmail.com>
2026-08-16 17:15:31 -0300

    ejercicio 1A

---
+++ guia_c_avanzada/ejercicio_1.c
@@ -0,0 +1,27 @@
+
+#include <stdio.h>
+
+typedef struct {
+    char nombre[50];
+    int vida;
+    double ataque;
+    double defensa;
+} mounstruo_t;
+
+int main() {
+    mounstruo_t mounstruos[3] = {
+        {"Goblin", 10, 1.0, 0.0},
+        {"Ogro", 25, 12, 2.5},
+        {"Zombie", 15, 2.0, 1.5}};
+    for(int i = 0; i < 3; i++) {
+        printf("Mounstruo #%d: %s\n"
+        "Vida: %d\n"
+        "Ataque: %.2lf\n"
+        "Defensa: %.2lf\n",
+        i + 1, mounstruos[i].nombre,
+        mounstruos[i].vida,
+        mounstruos[i].ataque,
+        mounstruos[i].defensa);
+    }
+    return 0;
+}
