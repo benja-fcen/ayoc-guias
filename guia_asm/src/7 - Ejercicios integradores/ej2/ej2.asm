1537479841ec7721d3b62f8e0240ad10cc659a8b
benja-fcen <rtb.fcen@gmail.com>
2026-08-27 14:27:36 -0300

    ejercicio 2A integrador

--- guia_asm/src/7 - Ejercicios integradores/ej2/ej2.asm
+++ guia_asm/src/7 - Ejercicios integradores/ej2/ej2.asm
@@ -1,5 +1,5 @@
 extern malloc
-
+extern free 
 section .rodata
 ; Acá se pueden poner todas las máscaras y datos que necesiten para el ejercicio
 
@@ -14,7 +14,7 @@ TRUE  EQU 1
 ; Funciones a implementar:
 ;   - optimizar
 global EJERCICIO_2A_HECHO
-EJERCICIO_2A_HECHO: db FALSE ; Cambiar por `TRUE` para correr los tests.
+EJERCICIO_2A_HECHO: db TRUE ; Cambiar por `TRUE` para correr los tests.
 
 ; Marca el ejercicio 1B como hecho (`true`) o pendiente (`false`).
 ;
@@ -32,10 +32,10 @@ EJERCICIO_2C_HECHO: db FALSE ; Cambiar por `TRUE` para correr los tests.
 
 ;########### ESTOS SON LOS OFFSETS Y TAMAÑO DE LOS STRUCTS
 ; Completar las definiciones (serán revisadas por ABI enforcer):
-ATTACKUNIT_CLASE EQU ??
-ATTACKUNIT_COMBUSTIBLE EQU ??
-ATTACKUNIT_REFERENCES EQU ??
-ATTACKUNIT_SIZE EQU ??
+ATTACKUNIT_CLASE EQU 0
+ATTACKUNIT_COMBUSTIBLE EQU 12
+ATTACKUNIT_REFERENCES EQU 14
+ATTACKUNIT_SIZE EQU 16
 
 global optimizar
 optimizar:
@@ -46,6 +46,44 @@ optimizar:
 	; r/m64 = mapa_t           mapa
 	; r/m64 = attackunit_t*    compartida
 	; r/m64 = uint32_t*        fun_hash(attackunit_t*)
+  push rbp
+  mov rbp, rsp
+  sub rsp, 48
+  mov [rbp - 8], rdi          ; mapa
+  mov [rbp - 16], rsi         ; compartida
+  mov [rbp - 24], rdx         ; fun_hash
+  mov QWORD [rbp - 32], 0           ; i
+  mov BYTE [rbp - 40], 0           ; fun_hash(compartida)
+  mov rdi, rsi
+  call [rbp - 24]
+  mov [rbp - 40], ax
+  .l0:
+    mov rcx, [rbp - 32]       ; i
+    mov rdi, [rbp - 8]        ; mapa
+    mov rdi, [rdi + 8 * rcx]  ; cUnit = mapa[i]
+    test rdi, rdi
+    jz .continue
+    call [rbp - 24]           ;
+    sub ax, [rbp - 40]        ;
+    jnz .continue             ;
+    mov rcx, [rbp - 32]       ; i
+    mov r8, [rbp - 8]         ; mapa
+    mov rsi, [r8 + 8 * rcx]   ; cUnit = mapa[i]
+    mov rdx, [rbp - 16]       ; compartida
+
+    mov rcx, ATTACKUNIT_REFERENCES   
+    mov [r8 + 8 * rcx], rdx   ; mapa[i] = compartida
+    inc BYTE [rdx + rcx]      ; compartida->references++
+    dec BYTE [rsi + rcx]      ; cUnit->references--
+    jnz .continue
+    call free
+    .continue:
+    inc QWORD [rbp - 32]
+  .c0:
+    test QWORD [rbp - 32], 256 * 256
+    jz .l0
+  mov rsp, rbp
+  pop rbp
 	ret
 
 global contarCombustibleAsignado
--- guia_asm/src/7 - Ejercicios integradores/ej2/ej2.c
+++ guia_asm/src/7 - Ejercicios integradores/ej2/ej2.c
@@ -10,7 +10,7 @@
  * Funciones a implementar:
  *   - es_indice_ordenado
  */
-bool EJERCICIO_2A_HECHO = false;
+bool EJERCICIO_2A_HECHO = true;
 
 /**
  * Marca el ejercicio 1B como hecho (`true`) o pendiente (`false`).
@@ -31,17 +31,31 @@ bool EJERCICIO_2C_HECHO = false;
 /**
  * OPCIONAL: implementar en C
  */
-void optimizar(mapa_t mapa, attackunit_t* compartida, uint32_t (*fun_hash)(attackunit_t*)) {
+void optimizar(mapa_t mapa, attackunit_t *compartida,
+               uint32_t (*fun_hash)(attackunit_t *)) {
+  for (int i = 0; i < 255; i++)
+    for (int j = 0; j < 255; j++) {
+      attackunit_t *cUnit = mapa[i][j];
+      if (cUnit) {
+        if (fun_hash(cUnit) == fun_hash(compartida)) {
+          mapa[i][j] = compartida;
+          compartida->references++;
+          cUnit->references--;
+        }
+        if (cUnit->references == 0)
+          free(cUnit);
+      }
+    }
 }
 
 /**
  * OPCIONAL: implementar en C
  */
-uint32_t contarCombustibleAsignado(mapa_t mapa, uint16_t (*fun_combustible)(char*)) {
-}
+uint32_t contarCombustibleAsignado(mapa_t mapa,
+                                   uint16_t (*fun_combustible)(char *)) {}
 
 /**
  * OPCIONAL: implementar en C
  */
-void modificarUnidad(mapa_t mapa, uint8_t x, uint8_t y, void (*fun_modificar)(attackunit_t*)) {
-}
+void modificarUnidad(mapa_t mapa, uint8_t x, uint8_t y,
+                     void (*fun_modificar)(attackunit_t *)) {}
