216b7a96535696c3d5eb2ad448a4ab36bd8471d6
benja-fcen <rtb.fcen@gmail.com>
2026-08-30 00:19:14 -0300

    ejercicio 2 integrador

--- guia_asm/src/7 - Ejercicios integradores/ej2/ej2.asm
+++ guia_asm/src/7 - Ejercicios integradores/ej2/ej2.asm
@@ -1,5 +1,6 @@
 extern malloc
-extern free 
+extern free
+extern memcpy
 section .rodata
 ; Acá se pueden poner todas las máscaras y datos que necesiten para el ejercicio
 
@@ -21,14 +22,14 @@ EJERCICIO_2A_HECHO: db TRUE ; Cambiar por `TRUE` para correr los tests.
 ; Funciones a implementar:
 ;   - contarCombustibleAsignado
 global EJERCICIO_2B_HECHO
-EJERCICIO_2B_HECHO: db FALSE ; Cambiar por `TRUE` para correr los tests.
+EJERCICIO_2B_HECHO: db TRUE ; Cambiar por `TRUE` para correr los tests.
 
 ; Marca el ejercicio 1C como hecho (`true`) o pendiente (`false`).
 ;
 ; Funciones a implementar:
 ;   - modificarUnidad
 global EJERCICIO_2C_HECHO
-EJERCICIO_2C_HECHO: db FALSE ; Cambiar por `TRUE` para correr los tests.
+EJERCICIO_2C_HECHO: db TRUE ; Cambiar por `TRUE` para correr los tests.
 
 ;########### ESTOS SON LOS OFFSETS Y TAMAÑO DE LOS STRUCTS
 ; Completar las definiciones (serán revisadas por ABI enforcer):
@@ -49,39 +50,38 @@ optimizar:
   push rbp
   mov rbp, rsp
   sub rsp, 48
-  mov [rbp - 8], rdi          ; mapa
-  mov [rbp - 16], rsi         ; compartida
-  mov [rbp - 24], rdx         ; fun_hash
-  mov QWORD [rbp - 32], 0           ; i
-  mov BYTE [rbp - 40], 0           ; fun_hash(compartida)
-  mov rdi, rsi
-  call [rbp - 24]
-  mov [rbp - 40], ax
-  .l0:
-    mov rcx, [rbp - 32]       ; i
-    mov rdi, [rbp - 8]        ; mapa
-    mov rdi, [rdi + 8 * rcx]  ; cUnit = mapa[i]
-    test rdi, rdi
-    jz .continue
-    call [rbp - 24]           ;
-    sub ax, [rbp - 40]        ;
-    jnz .continue             ;
-    mov rcx, [rbp - 32]       ; i
-    mov r8, [rbp - 8]         ; mapa
-    mov rsi, [r8 + 8 * rcx]   ; cUnit = mapa[i]
-    mov rdx, [rbp - 16]       ; compartida
+  mov [rbp - 8], rdi                        ; mapa
+  mov [rbp - 16], rsi                       ; compartida
+  mov [rbp - 24], rdx                       ; fun_hash
+  mov qword [rbp - 32], 0                   ; i
+  ;mov dword [rbp - 40], 0                  ; fun_hash(compartida)
+  mov rdi, rsi                              ; rdi = compartida
+  call [rbp - 24]                           ; fun_hash(rdi)
+  mov [rbp - 40], eax                       ; eax = fun_hash(compartida)
+  .l0:                                      ; for(int i = 0; i < 255 * 255; i--)
+    mov rcx, [rbp - 32]                     ; i
+    mov rdi, [rbp - 8]                      ; mapa
+    mov rdi, [rdi + 8 * rcx]                ; rdi = cUnit = mapa[i]
+    test rdi, rdi                           ; if(cUnit == NULL)
+    jz .continue                            ;   continue;
+    call [rbp - 24]                         ;
+    cmp eax, [rbp - 40]                     ; if(fun_hash(cUnit) != fun_hash(compartida))
+    jne .continue                           ;   continue;
+    mov rcx, [rbp - 32]                     ; i
+    mov r8, [rbp - 8]                       ; mapa
+    mov rdi, [r8 + 8 * rcx]                 ; cUnit = mapa[i]
+    mov rsi, [rbp - 16]                     ; compartida
 
-    mov rcx, ATTACKUNIT_REFERENCES   
-    mov [r8 + 8 * rcx], rdx   ; mapa[i] = compartida
-    inc BYTE [rdx + rcx]      ; compartida->references++
-    dec BYTE [rsi + rcx]      ; cUnit->references--
-    jnz .continue
+    inc byte [rsi + ATTACKUNIT_REFERENCES]  ; compartida->references++
+    dec byte [rdi + ATTACKUNIT_REFERENCES]  ; cUnit->references--
+    mov [r8 + 8 * rcx], rsi                 ; mapa[i] = compartida
+    jnz .continue                           ; cUnit->references ? continue : free(cUnit)
     call free
     .continue:
-    inc QWORD [rbp - 32]
   .c0:
-    test QWORD [rbp - 32], 256 * 256
-    jz .l0
+    inc QWORD [rbp - 32]
+    cmp QWORD [rbp - 32], 255 * 255
+    jb .l0
   mov rsp, rbp
   pop rbp
 	ret
@@ -90,6 +90,35 @@ global contarCombustibleAsignado
 contarCombustibleAsignado:
 	; r/m64 = mapa_t           mapa
 	; r/m64 = uint16_t*        fun_combustible(char*)
+  push rbp
+  mov rbp, rsp
+  sub rsp, 32
+  mov [rbp - 8], rdi                              ; mapa
+  mov [rbp - 16], rsi                             ; fun
+  mov qword [rbp - 24], 0                         ; i
+  mov dword [rbp - 32], 0                         ; total
+  .l0:                                            ; for(int i = 0; i < 255 * 255; i++)
+    mov rdi, [rbp - 8]                            ; mapa
+    ;mov rsi, [rbp - 16]                          ; fun
+    mov rdx, [rbp - 24]                           ; i
+    mov rdi, [rdi + 8 * rdx]                      ; mapa[i]
+    test rdi, rdi                                 ; if(mapa[i] == NULL) continue
+    jz .continue                                  ;   continue
+    ;mov rdi, [rdi]                               ; rdi = cUnit->clase
+    movzx esi, word [rdi + ATTACKUNIT_COMBUSTIBLE]; esi = cUnit->combustible
+    add [rbp - 32], esi                           ; total += cUnit->combustible
+    mov rax, [rdi + ATTACKUNIT_CLASE]             ; rax = cUnit->clase
+    call [rbp - 16]                               ; fun_combustible(cUnit->clase)
+    movzx eax, ax                                 ;
+    sub [rbp - 32], eax                           ; total -= fun_combustible(rdi->clase)
+  .continue:
+    inc qword [rbp - 24]
+    cmp qword [rbp - 24], 255 * 255
+    jb .l0
+
+  mov eax, [rbp - 32]
+  mov rsp, rbp
+  pop rbp
 	ret
 
 global modificarUnidad
@@ -98,4 +127,48 @@ modificarUnidad:
 	; r/m8  = uint8_t          x
 	; r/m8  = uint8_t          y
 	; r/m64 = void*            fun_modificar(attackunit_t*)
+  push rbp
+  mov rbp, rsp
+  sub rsp, 48
+
+  movzx rsi, sil
+  mov rax, rsi                              ; rax = x
+  sal rax, 8                                ; rax = x * 256
+  sub rax, rsi                              ; rax = rax - x = x * 255
+  movzx rdx, dl
+  add rax, rdx                              ; rax = x * 255 + y
+  mov [rbp - 8], rdi                        ; mapa
+  mov [rbp - 16], rax                       ; x * 255 + y
+  mov [rbp - 24], rcx                       ; fun_modificar
+  ;mov qword [rbp - 32], 0                  ; attackunit_t *cUnit
+  ;mov qword [rbp - 40], 0                  ; attackunit_t *nUnit
+
+  mov rdi, [rdi + 8 * rax]                  ; rdi = cUnit = mapa[x][y]
+  mov [rbp - 32], rdi
+  test rdi, rdi                             ; if(!cUnit)
+  jz .return                                ;   return;
+  mov sil, [rdi + ATTACKUNIT_REFERENCES]    ;
+  cmp sil, 1                                ; if(cUnit->references == 1)
+  je .modificar                             ;   fun_modificar(cUnit);
+                                            ; else {...
+  mov rdi, ATTACKUNIT_SIZE                  ; rdi = sizeof(attackunit_t)
+  call malloc                               ; malloc(sizeof(attackunit_t))
+  mov [rbp - 40], rax                       ; nUnit = malloc(sizeof(attackunit_t))
+  mov rdi, rax                              ; rdi = nUnit
+  mov rsi, [rbp - 32]                       ; rsi = cUnit
+  mov rdx, ATTACKUNIT_SIZE                  ; rdx = sizeof(attackunit_t)
+  call memcpy                               ; memcpy(rdi, rsi, sizeof(attackunit_t))
+  mov rdi, [rbp - 32]                       ; rdi = cUnit
+  mov byte [rax + ATTACKUNIT_REFERENCES], 1 ; nUnit->references = 1
+  sub byte [rdi + ATTACKUNIT_REFERENCES], 1 ; cUnit->references--
+  mov rsi, [rbp - 8]                        ; rsi = mapa
+  mov rdx, [rbp - 16]                       ; rdx = x * 255 + y
+  mov [rsi + 8 * rdx], rax                  ; mapa[x][y] = nUnit
+  mov rdi, rax                              ; rdi = nUnit
+  .modificar:
+  call [rbp - 24]                           ; fun_modificar(rdi)
+
+  .return:
+  mov rsp, rbp
+  pop rbp
 	ret
--- guia_asm/src/7 - Ejercicios integradores/ej2/ej2.c
+++ guia_asm/src/7 - Ejercicios integradores/ej2/ej2.c
@@ -18,7 +18,7 @@ bool EJERCICIO_2A_HECHO = true;
  * Funciones a implementar:
  *   - contarCombustibleAsignado
  */
-bool EJERCICIO_2B_HECHO = false;
+bool EJERCICIO_2B_HECHO = true;
 
 /**
  * Marca el ejercicio 1B como hecho (`true`) o pendiente (`false`).
@@ -26,7 +26,7 @@ bool EJERCICIO_2B_HECHO = false;
  * Funciones a implementar:
  *   - modificarUnidad
  */
-bool EJERCICIO_2C_HECHO = false;
+bool EJERCICIO_2C_HECHO = true;
 
 /**
  * OPCIONAL: implementar en C
@@ -52,10 +52,34 @@ void optimizar(mapa_t mapa, attackunit_t *compartida,
  * OPCIONAL: implementar en C
  */
 uint32_t contarCombustibleAsignado(mapa_t mapa,
-                                   uint16_t (*fun_combustible)(char *)) {}
+                                   uint16_t (*fun_combustible)(char *)) {
+  uint32_t total = 0;
+  for (int i = 0; i < 255; i++)
+    for (int j = 0; j < 255; j++) {
+      attackunit_t *cUnit = mapa[i][j];
+      if (!cUnit)
+        continue;
+      total += cUnit->combustible - fun_combustible(cUnit->clase);
+    }
+  return total;
+}
 
 /**
  * OPCIONAL: implementar en C
  */
 void modificarUnidad(mapa_t mapa, uint8_t x, uint8_t y,
-                     void (*fun_modificar)(attackunit_t *)) {}
+                     void (*fun_modificar)(attackunit_t *)) {
+  attackunit_t *cUnit = mapa[x][y];
+  if (!cUnit)
+    return;
+  if (cUnit->references == 1)
+    fun_modificar(cUnit);
+  else {
+    attackunit_t *nUnit = malloc(sizeof(attackunit_t));
+    memcpy(nUnit, cUnit, sizeof(attackunit_t));
+    nUnit->references = 1;
+    cUnit->references--;
+    mapa[x][y] = nUnit;
+    fun_modificar(nUnit);
+  }
+}
