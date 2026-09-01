d81617422778c313c7d3d57602cbc6865a86cf14
benja-fcen <rtb.fcen@gmail.com>
2026-09-01 16:21:09 -0300

    ejercicio 4 integrador

--- guia_asm/src/7 - Ejercicios integradores/ej4/ej4a/ej4a.asm
+++ guia_asm/src/7 - Ejercicios integradores/ej4/ej4a/ej4a.asm
@@ -19,26 +19,26 @@ TRUE  EQU 1
 ; Funciones a implementar:
 ;   - init_fantastruco_dir
 global EJERCICIO_1A_HECHO
-EJERCICIO_1A_HECHO: db FALSE ; Cambiar por `TRUE` para correr los tests.
+EJERCICIO_1A_HECHO: db TRUE ; Cambiar por `TRUE` para correr los tests.
 
 ; Marca el ejercicio 1B como hecho (`true`) o pendiente (`false`).
 ;
 ; Funciones a implementar:
 ;   - summon_fantastruco
 global EJERCICIO_1B_HECHO
-EJERCICIO_1B_HECHO: db FALSE ; Cambiar por `TRUE` para correr los tests.
+EJERCICIO_1B_HECHO: db TRUE ; Cambiar por `TRUE` para correr los tests.
 
 ;########### ESTOS SON LOS OFFSETS Y TAMAÑO DE LOS STRUCTS
 ; Completar las definiciones (serán revisadas por ABI enforcer):
-DIRENTRY_NAME_OFFSET EQU ??
-DIRENTRY_PTR_OFFSET EQU ??
-DIRENTRY_SIZE EQU ??
+DIRENTRY_NAME_OFFSET EQU 0
+DIRENTRY_PTR_OFFSET EQU 16
+DIRENTRY_SIZE EQU 24
 
-FANTASTRUCO_DIR_OFFSET EQU ??
-FANTASTRUCO_ENTRIES_OFFSET EQU ??
-FANTASTRUCO_ARCHETYPE_OFFSET EQU ??
-FANTASTRUCO_FACEUP_OFFSET EQU ??
-FANTASTRUCO_SIZE EQU ??
+FANTASTRUCO_DIR_OFFSET EQU 0
+FANTASTRUCO_ENTRIES_OFFSET EQU 8
+FANTASTRUCO_ARCHETYPE_OFFSET EQU 16
+FANTASTRUCO_FACEUP_OFFSET EQU 24
+FANTASTRUCO_SIZE EQU 32
 
 ; void init_fantastruco_dir(fantastruco_t* card);
 global init_fantastruco_dir
@@ -48,12 +48,59 @@ init_fantastruco_dir:
 	; valores son de 64 bits y qué valores son de 32 bits o 8 bits.
 	;
 	; r/m64 = fantastruco_t*     card
+  push rbp
+  mov rbp, rsp
+  push r12      ; card
+  push r13      ; dir
+  mov r12, rdi
+  
+  ; creacion de dir
+  mov rdi, DIRENTRY_SIZE * 2
+  call malloc
+  mov r13, rax
+  mov rdi, sleep_name
+  mov rsi, sleep
+  call create_dir_entry
+  mov [r13], rax
+  mov rdi, wakeup_name
+  mov rsi, wakeup
+  call create_dir_entry
+  mov [r13 + 8], rax
+  
+  ; seteo de campos de card
+  mov [r12 + FANTASTRUCO_DIR_OFFSET], r13
+  mov word [r12 + FANTASTRUCO_ENTRIES_OFFSET], 2
 
+  pop r13
+  pop r12
+  mov rsp, rbp
+  pop rbp
 	ret ;No te olvides el ret!
 
 ; fantastruco_t* summon_fantastruco();
 global summon_fantastruco
 summon_fantastruco:
 	; Esta función no recibe parámetros
+  push rbp
+  mov rbp, rsp
+  sub rsp, 8
+  push r12
+  mov rdi, FANTASTRUCO_SIZE
+  
+  ; creacion de carta
+  call malloc
+  mov r12, rax
 
+  ; seteo de campos
+  mov qword [rax + FANTASTRUCO_ARCHETYPE_OFFSET], 0
+  mov byte [rax + FANTASTRUCO_FACEUP_OFFSET], 1
+  mov rdi, rax
+  call init_fantastruco_dir
+
+  ; devolvemos carta
+  mov rax, r12
+  
+  pop r12
+  mov rsp, rbp
+  pop rbp
 	ret ;No te olvides el ret!
--- guia_asm/src/7 - Ejercicios integradores/ej4/ej4a/ej4a.c
+++ guia_asm/src/7 - Ejercicios integradores/ej4/ej4a/ej4a.c
@@ -1,3 +1,4 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -10,11 +11,16 @@
  * Funciones a implementar:
  *   - init_fantastruco_dir
  */
-bool EJERCICIO_1A_HECHO = false;
+bool EJERCICIO_1A_HECHO = true;
 
 // OPCIONAL: implementar en C
-void init_fantastruco_dir(fantastruco_t* card) {
-
+void init_fantastruco_dir(fantastruco_t* card) { 
+  directory_t dir = malloc(sizeof(directory_entry_t*) * 2);
+  dir[0] = create_dir_entry("sleep", sleep);
+  dir[1] = create_dir_entry("wakeup", wakeup);
+  card->__dir = dir;
+  card->__archetype = NULL;
+  card->__dir_entries = 2;
 }
 
 /**
@@ -23,9 +29,12 @@ void init_fantastruco_dir(fantastruco_t* card) {
  * Funciones a implementar:
  *   - summon_fantastruco
  */
-bool EJERCICIO_1B_HECHO = false;
+bool EJERCICIO_1B_HECHO = true;
 
 // OPCIONAL: implementar en C
 fantastruco_t* summon_fantastruco() {
-
+  fantastruco_t* carta = malloc(sizeof(fantastruco_t));
+  carta->face_up = true;
+  init_fantastruco_dir(carta);
+  return carta;
 }
